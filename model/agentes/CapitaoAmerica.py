32ff2d24c01357693daf6987e5925f77ec966f1f
Herikles Cordeiro <herikles.cordeiro@gmail.com>
2019-04-28 21:00:47 -0300

    passando o agente capitaoAmerica para um classe

--- model/agentes/CapitaoAmerica.py
+++ model/agentes/CapitaoAmerica.py
@@ -1,40 +1,41 @@
 import xml.etree.ElementTree as et
+class CapitaoAmerica:
+    def __init__(self):
+        self._arquivoXML = 'model/arquivos/bases/baseDados.xml'
+        self._arvore = et.parse(self._arquivoXML)
+        self.raiz = self._arvore.getroot()
 
-arquivoXML = 'model/arquivos/bases/baseDados.xml'
-arvore = et.parse(arquivoXML)
-raiz = arvore.getroot()
+    def _retornarResposta(self, nome, acusacao):
+        if len(self._retornarFN(self._retornarVacina(nome), acusacao))>0:
+            return self._retornarFN(self._retornarVacina(nome), acusacao)[3].text, self._retornarFN(self._retornarVacina(nome), acusacao)[4].text
+        return ''
 
-def _retornarResposta(nome, acusacao):
-    if len(_retornarFN(_retornarVacina(nome), acusacao))>0:
-        return _retornarFN(_retornarVacina(nome), acusacao)[3].text, _retornarFN(_retornarVacina(nome), acusacao)[4].text
-    return ''
+    def _retornarDescricao(self,vacina,filho):
+        aux = [i for i in self._retornarVacina(vacina) if i.tag == filho]
+        if len(self._retornarVacina(vacina))>0 and len(aux) > 0:
+            return aux[0].text
+        return ''
 
-def _retornarDescricao(vacina,filho):
-    aux = [i for i in _retornarVacina(vacina) if i.tag == filho]
-    if len(_retornarVacina(vacina))>0 and len(aux) > 0:
-        return aux[0].text
-    return ''
-
-def _retornarVacina(nome):
-    for x in raiz:
-        if nome.lower() == (x.attrib['nome']).lower() or nome.lower() == (x.attrib['sigla']).lower():
-            return x
-    return '' 
-
-def _retornarFN(id,acusacao):
-    if id != '':
-        nunFN = [i for i in id if i.tag =='fakenews'][0]
-        for x in nunFN:
-            if acusacao.lower() == (x[0].text).lower() or acusacao.lower() == (x[1].text).lower() or acusacao.lower() == (x[2].text).lower():
+    def _retornarVacina(self,nome):
+        for x in self.raiz:
+            if nome.lower() == (x.attrib['nome']).lower() or nome.lower() == (x.attrib['sigla']).lower():
                 return x
-    return ''
+        return '' 
 
-def agenteCapitaoAmerica(vacina, frase):
-    tempFN = _retornarResposta(vacina,frase)
-    tempDescri = _retornarDescricao(vacina,frase)
+    def _retornarFN(self,id,acusacao):
+        if id != '':
+            nunFN = [i for i in id if i.tag =='fakenews'][0]
+            for x in nunFN:
+                if acusacao.lower() == (x[0].text).lower() or acusacao.lower() == (x[1].text).lower() or acusacao.lower() == (x[2].text).lower():
+                    return x
+        return ''
 
-    if(tempFN!=''):
-        return tempFN
-    if(tempDescri!=''):
-        return tempDescri
-    return ''
+    def agenteCapitaoAmerica(self, vacina, frase):
+        tempFN = self._retornarResposta(vacina,frase)
+        tempDescri = self._retornarDescricao(vacina,frase)
+
+        if(tempFN!=''):
+            return tempFN
+        if(tempDescri!=''):
+            return tempDescri
+        return ''
