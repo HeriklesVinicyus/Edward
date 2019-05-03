5176d7c05068702bd43304e7519548128381808a
Herikles Cordeiro <herikles.cordeiro@gmail.com>
2019-05-02 23:46:22 -0300

    Alteracao nas classes Main e Pasquale
    Na classe Pasquale o metodo tratadas foi alterado para adicionar um id para cada frase e cada frase tratada recebe o mesmo id da frase oricinal. na classe Main o metodo _verificarFrases foi criado para separa um bloco de texto que estava dentro de _agir, assim organizando melhor; o metodo verificarTexto foi criado para a analisar o texto de entrada e retornar um lista de com acusacoes [respostas e fontes]; o metodo responder foi criado para retorna um str para a saida em um formado compreensivo para pessoas

--- model/Main.py
+++ model/Main.py
@@ -7,25 +7,43 @@ class Edward:
         self._ag1 = Pasquale()
         self._ag2 = Ramalho()
         self._ag3 = CapitaoAmerica()
+        self.acusacao = []
         self.resposta = []
 
-    def _agir(self, frase):
-        frases = self._ag1.tratadas(frase)
-        cont = len(frases)-1
-        while(cont>=0):
-            aux = self._ag2.retornaFaseComPalavrasConhecidas(frases[cont])
-            frases.append(aux)
-            cont-=1
-
+    def _verificarFrases(self, frases):
         for i in frases:
-            aux = self._ag3.agenteCapitaoAmerica(i)
+            aux = self._ag3.agenteCapitaoAmerica(i[1])
             if aux != '' and [aux[0],aux[1]] not in self.resposta:
+                for j in frases:
+                    if j[0] == i[0]:
+                        self.acusacao.append(j[1])
+                        break
                 self.resposta.append([aux[0],aux[1]])
-        return self.resposta
 
+    def _agir(self, frase):
+        frasesTradadascomIDs = self._ag1.tratadas(frase)
+        cont = len(frasesTradadascomIDs)-1
+        while(cont>=0):
+            aux = self._ag2.retornaFaseComPalavrasConhecidas(frasesTradadascomIDs[cont][1])
+            frasesTradadascomIDs.append([frasesTradadascomIDs[cont][0],aux])
+            cont-=1
+        self._verificarFrases(frasesTradadascomIDs)   
+        return self.acusacao,self.resposta
+    
+    def verificarTexto(self,frase):
+        resp = []
+        aux = self._agir(frase)
+        for i in range(len(aux[0])):
+            resp.append([aux[0][i],aux[1][i]])
+        return resp
+    
+    def responder(self, arrayDeAcusacoesRespostas):
+        resp = ''
+        for i in arrayDeAcusacoesRespostas:
+            resp+= 'Sobre a frase: {}\nDeclaro: {}\n<Fonte para mais pesquisas: {}>\n\n'.format(i[0],i[1][0],i[1][1])
+        return resp
 
 test = Edward()
-nosso = test._agir('Vacina contra o HPV da a incentiva o sexo sem preservativo. Vacina do HPV UM encorajamento PRECOCE À VIDA SEXUAL')
+nosso = test.verificarTexto('meu filho não tem casa. Conservantes nas vacinas não faz bem. eu ouvi dizer que a Vacina contra o HPV da a incentiva o sexo sem preservativo? Vacina do HPV UM encorajamento PRECOCE À VIDA SEXUAL')
 
-for i in nosso:
-    print(i)
+print (test.responder(nosso))
--- model/agentes/Pasquale.py
+++ model/agentes/Pasquale.py
@@ -16,9 +16,11 @@ class Pasquale:
       return (' '.join([i.lemma_ for i in self._nlp(str(frase)) if not i.is_punct])).lower()
    
    def tratadas(self, frase):
-      resp = [frase]
+      resp = [[0,frase]]
+      cont = 1
       for i in nltk.tokenize.sent_tokenize(frase):
-         resp.append(i)
-         resp.append(self._tratamentoSpacy(i))
-         resp.append(self. _tratamentoNltk(i))
+         resp.append([cont,i])
+         resp.append([cont, self._tratamentoSpacy(i)])
+         resp.append([cont,self. _tratamentoNltk(i)])
+         cont+=1
       return resp
