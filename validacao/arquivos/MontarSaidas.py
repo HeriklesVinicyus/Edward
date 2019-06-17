92ca2c9d497c5fdcbea9d726083245f2c5a0857d
Herikles Cordeiro <herikles.cordeiro@gmail.com>
2019-06-17 01:39:07 -0300

    Organização do pacote validacao
    Alteração do nome do pacote ‘validar’ para ‘validacao’; Remoção da classe ‘Validador.py’; Criação da classe ‘MontarSaidas.py’ equivalente a classe ‘Validador.py’; Renomeação das funções;  Criação da função ‘executar()’: função para recompilar o arquivo de saída(deve ser executada fora da classe)

---
+++ validacao/arquivos/MontarSaidas.py
@@ -0,0 +1,64 @@
+#class MotarSaidas
+
+'''
+imports necessário para acessar a pasta controle
+'''
+import sys
+import os
+caminho = os.getcwd()
+sys.path.append(caminho)
+
+import xml.etree.ElementTree as et
+#importação do controle
+from controle.Edward import Edward
+
+#Armazena o parâmetro no arquivo de saídas
+def _adcionarSaida(acusacao, respostaFonte):
+    _arquivoXML = 'validar/Saida.xml'
+    _arvore = et.parse(_arquivoXML)
+    _raiz = _arvore.getroot()
+    _novaSaida = et.SubElement(_raiz,'saida')
+    _novaAcusacao = et.SubElement(_novaSaida,'acusacao')
+    _resposta = et.SubElement(_novaSaida,'resposta')
+    _fonte = et.SubElement(_novaSaida,'fonte')
+    _novaAcusacao.text = acusacao
+    _resposta.text = respostaFonte[0]
+    _fonte.text = respostaFonte[1]
+    _arvore.write(_arquivoXML)
+
+#Retorna as frases do arquivo de entradas
+def _buscarEntradas():
+    _resp = ''
+    _arquivoXML = 'validar/Entrada.xml'
+    _arvore = et.parse(_arquivoXML)
+    _raiz = _arvore.getroot()
+    for i in _raiz:
+        _resp += i.text+'\n'
+    return _resp
+
+#Armazena no arquivo de saídas lista com varias saídas
+def _adcionarVariasSaidas(lista):
+        _arquivoXML = 'validar/Saida.xml'
+        _arvore = et.parse(_arquivoXML)
+        _raiz = _arvore.getroot()
+        cont = len(_raiz)-1
+        for i in range(cont,-1,-1):
+                _raiz.remove(_raiz.getchildren()[i])
+        _arvore.write(_arquivoXML)
+                
+        for i in lista:
+                _adcionarSaida(i[0][0],i[0][1])
+
+#Função para recompilar o arquivo de saída
+#Única função acessível fora dessa classe
+def executar():
+        entradas = _buscarEntradas()
+        lista = []
+        cont = 0
+        for i in entradas.split('\n'):
+                aux =Edward().verificarTextoEspscifico(i)
+                if len(aux) != 0:
+                        lista.append(aux)
+                cont+=1
+
+        _adcionarVariasSaidas(lista)
